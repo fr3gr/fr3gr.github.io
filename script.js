b2744cbc1453574f8003234bd3bd95775d1b5436
fr3gr <kazi.fregr@gmail.com>
2024-10-15 02:35:01 +0200

    a

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Funkcja do wysuwania i chowania menu
 function toggleMenu() {
-    const menu = document.getElementById('menu');
+    const menu = document.querySelector('nav');
     menu.classList.toggle('active');
 }
 
+// Event listener dla przycisku menu
 document.querySelector('.menu-btn').addEventListener('click', toggleMenu);
